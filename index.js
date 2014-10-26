66c88614d46621d8d858aa96fe4b1b6a6b87f5ba
Richard Walker <digitalsadhu@gmail.com>
2014-10-26 17:07:59 +0100

    Add documentation

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ var intersection = require('robust-segment-intersect')
 var assert       = require('assert')
 var util         = require('util')
 
+/**
+ * Determines if 2 params line1 and line2 intersect.
+ * Determines intersection by looping through each node pair of line1 and
+ * comparing that against each of the node pairs in line2 using
+ * the robust-segment-intersect module
+ * @param  {array} line1 - @example [[1,2],[3,4],[3,5]]
+ * @param  {array} line2 - @example [[1,2],[3,4],[3,5]]
+ * @return {boolean}
+ */
 module.exports = function intersects(line1, line2) {
   assert(util.isArray(line1), 'argument `line1` must be an array')
   assert(util.isArray(line2), 'argument `line2` must be an array')
